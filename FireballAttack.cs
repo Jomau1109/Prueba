760c7098568c7cd1d171a9158447ba77ddd0f1cf
Jomau1109 <jomau.vc11@gmail.com>
2025-10-18 10:43:27 -0600

    Add files via upload

---
+++ FireballAttack.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballAttack : MonoBehaviour
+{
+    //The Fireball prefab and the Transform parameter of the attack point
+    public GameObject fireballPrefab;
+    public Transform attackPoint;
+    public Camera playerCamera;
+
+
+    void Update()
+    {
+        //If the player clicks the left mouse button, a fireball is created
+        if (Input.GetMouseButtonDown(0))
+        {
+            ShootFireball();
+        }
+    }
+
+    void ShootFireball()
+    {
+        Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        Vector3 targetPoint = Physics.Raycast(ray, out RaycastHit hit) ? hit.point : ray.GetPoint(1000);
+
+        GameObject fireball = Instantiate(
+            fireballPrefab, 
+            attackPoint.position, 
+            Quaternion.LookRotation(targetPoint - attackPoint.position)
+            );
+    }
+    
+}
